bf693165f41dad0802d3470d94a69cfe27b54cb4
658146603 <a1533144693@163.com>
2018-12-03 22:36:03 +0800

    2018.12.03 t004-t005

---
+++ MyTeachingProject/t005.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+
+int main()
+{
+	int a=0, b=7;
+	int c, d;
+//	c=a+b; //左值 
+//	printf("%d\n", c);
+//	c=a/b;
+//	printf("%d\n", c);
+//	c=a*b;
+//	printf("%d\n", c);
+//	//float 精度/范围小于double
+//	//short int long      long long int
+//	
+//	c=a%b;
+//	printf("%d\n", c);
+	//a==0;
+	//d=(a++,b=a+7,c=6+a,++a);
+	
+	//printf("%d\n", a++);
+	//a==6
+	//printf("%d\n", a);
+	//printf("%d\n", ++a);
+	printf("%d\n", a=a+1);
+	
+	return 0;
+}
