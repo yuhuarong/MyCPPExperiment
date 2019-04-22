76e8f479bccae3278b8c9b12fa5ae9b3fd34311c
wcf <1533144693@qq.com>
2019-04-22 22:36:06 +0800

    update

--- Homework/Boolean/Boolean.cpp
+++ Homework/Boolean/Boolean.cpp
@@ -1,37 +1,79 @@
 #include<iostream>
 using namespace std;
 
-class Boolean{
-	public:
-		Boolean(){
-			flag=0;
+class Boolean {
+public:
+	Boolean() {
+		flag = 0;
+	}
+	Boolean(Boolean& b) {
+		flag = b.flag;
+	}
+
+	template<class T> Boolean(T flag) {
+		if (flag) {
+			this->flag = 1;
+		} else {
+			this->flag = 0;
 		}
-		template<class T>
-		Boolean(T flag){
-			if(flag!=0){
-				this->flag=1;
-			} else {
-				this->flag=0;
-			}
-		} 
-		
-		//friend ostream& operator<<(ostream& out, const Boolean& b);
-		int getFlag(){
-			return flag;
+	}
+
+	friend ostream& operator<<(ostream& out, const Boolean& b);
+
+	int getFlag() const {
+		return flag;
+	}
+
+	void setFlag(int i) {
+		if (flag) {
+			this->flag = 1;
 		}
-		void setFlag(int i){
-			if(flag){
-				this->flag=1;
-			} else {
-				this->flag=0;
-			}
+		else {
+			this->flag = 0;
 		}
-	private:
-		int flag;
+	}
+	
+	friend Boolean& operator^(const Boolean a, const Boolean b) {
+		Boolean* c;
+		c = new Boolean( (a.getFlag() && !b.getFlag()) || (!a.getFlag() && b.getFlag()) );
+		return *c;
+	}
+
+	Boolean& operator!() {
+		Boolean* a;
+		if (flag) {
+			a = new Boolean(0);
+		}
+		else
+		{
+			a = new Boolean(1);
+		}
+		return *a;
+	}
+
+	friend Boolean& operator&&(const Boolean& a, const Boolean& b) {
+		Boolean *c;
+		c = new Boolean(a.getFlag() && b.getFlag());
+		return *c;
+	}
+
+	friend Boolean& operator||(const Boolean& a, const Boolean& b) {
+		Boolean* c;
+		c = new Boolean(a.getFlag() || b.getFlag());
+		return *c;
+	}
+
+	Boolean& operator= (const Boolean& b) {
+		flag = b.getFlag();
+		return *this;
+	}
+
+	Boolean& operator= (bool b) {
+		flag = b;
+		return *this;
+	}
+
+private:
+	int flag;
 };
 
-
-ostream& operator << (ostream& out,Boolean& b){
-	out<<((b.getFlag()!=0)?"true":"false");
-	return out;
-}
--- Homework/Boolean/main.cpp
+++ Homework/Boolean/main.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
+//#include <iostream>
 #include "Boolean.cpp" 
-using namespace std;
+//using namespace std;
+
+ostream& operator<< (ostream& out, const Boolean& b) {
+	out << ((b.getFlag() != 0) ? "true" : "false");
+	return out;
+}
 
 int main() {
-	cout<<"helloworld"<<endl;
+	//cout << "helloworld" << endl;
 	Boolean a(0), b(1.10), c(a);
-	cout<<a<<endl<<b<<endl<<c<<endl;
+	cout << "a: " << a << " b: " << b << " c: " << c << endl;
+	a = true;
+	cout << "a: " << a << " b: " << b << " c: " << c << endl;
+	a = c && b;
+	cout << "a: " << a << " b: " << b << " c: " << c << endl;
+	a = b ^ b;
+	cout << "a: " << a << " b: " << b << " c: " << c << endl;
+	a = c ^ b;
+	cout << "a: " << a << " b: " << b << " c: " << c << endl;
+	a = c || b;
+	cout << "a: " << a << " b: " << b << " c: " << c << endl;
+	b = c;
+	cout << "a: " << a << " b: " << b << " c: " << c << endl;
+	c = Boolean(true);
+	cout << "a: " << a << " b: " << b << " c: " << c << endl;
 	return 0;
 }
