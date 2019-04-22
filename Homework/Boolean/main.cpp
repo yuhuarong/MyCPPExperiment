00ba333680bd075916eacf20778641c576cbc26e
wcf <1533144693@qq.com>
2019-04-22 11:59:02 +0800

    配置vscode示例

---
+++ Homework/Boolean/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"helloworld"<<endl;
+    char a[10];
+    cin>>a;
+}
---
+++ Test/helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+	cout<<"Helloworld\n";
+	int a;
+	cin>>a;
+	cout<<a;
+	
+	return 0;
+}
