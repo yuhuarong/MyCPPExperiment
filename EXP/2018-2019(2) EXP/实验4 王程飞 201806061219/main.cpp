702c41ed54d76365428125c6fe74435fd5bb7b93
wcf <1533144693@qq.com>
2019-04-25 18:37:02 +0800

    完成实验4

--- EXP/2018-2019(2) EXP/实验4 王程飞 201806061219/main.cpp
+++ EXP/2018-2019(2) EXP/实验4 王程飞 201806061219/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
 	ElementType a[10]={56,34,3,67,89,55,22,43,12,9};
+	
+	//初始化链表 
 	Node* list=new Node(a[0]);
 	Node* end=list;
 	for(int i=1;i<10;i++){
@@ -12,12 +14,13 @@ int main() {
 	} 
 	
 	Node* temp = list; 
-	for(int i=0;i<10;i++){
+	while(temp){
 		cout<<temp->data<<endl;
 		temp=temp->next;
 	}
 	cout<<endl;
 	
+	//89后插入72 
 	temp=list;
 	while(temp->data!=89){
 		temp=temp->next;
@@ -25,12 +28,13 @@ int main() {
 	Node* t=new Node(72,temp->next);
 	temp->next=t;
 	temp = list; 
-	for(int i=0;i<11;i++){
+	while(temp){
 		cout<<temp->data<<endl;
 		temp=temp->next;
 	}
 	cout<<endl;
 	
+	//22前插入90 
 	temp=list;
 	Node* former=temp; 
 	while(temp->data!=22){
@@ -40,22 +44,24 @@ int main() {
 	t=new Node(90,former->next);
 	former->next=t;
 	temp = list; 
-	for(int i=0;i<12;i++){
+	while(temp){
 		cout<<temp->data<<endl;
 		temp=temp->next;
 	}
 	cout<<endl;
 	
+	//链表首插入5 
 	temp=list;
 	t=new Node(5,list);
 	list=t; 
 	temp=list;
-	for(int i=0;i<13;i++){
+	while(temp){
 		cout<<temp->data<<endl;
 		temp=temp->next;
 	}
 	cout<<endl;
 	
+	//链表尾插入99 
 	temp=list;
 	while(temp->next){
 		temp=temp->next;
@@ -63,12 +69,13 @@ int main() {
 	t=new Node(99);
 	temp->next=t;
 	temp=list;
-	for(int i=0;i<14;i++){
+	while(temp){
 		cout<<temp->data<<endl;
 		temp=temp->next;
 	}
 	cout<<endl;
 	
+	//删除55 
 	temp=list;
 	former=temp;
 	while(temp->data!=55){
@@ -79,23 +86,61 @@ int main() {
 	former->next=temp->next;
 	delete t;
 	temp=list;
+	while(temp){
+		cout<<temp->data<<endl;
+		temp=temp->next;
+	}
+	cout<<endl;
+	
+	//逆序链表 
+	/*
+	temp=list;
+	list=temp->next;
+	temp->next=NULL;
+	Node* h;
+	while(list->next){
+		h=t;
+		t=list->next;
+		list->next=temp;
+		temp=list;
+		list=t;
+	}
+	list->next=h;
+	*/
+	temp=list;
+	list=NULL;
+	while(temp){
+		t=temp;
+		temp=temp->next;
+		t->next=list;
+		list=t; 
+	}
+	
+	temp=list;
 	for(int i=0;i<13;i++){
 		cout<<temp->data<<endl;
 		temp=temp->next;
 	}
 	cout<<endl;
 	
-	temp=list;
 	
-	t=temp->next;
-	temp->next=NULL;
-	Node* h=t; 
-	while(h!=NULL){
-		h=t->next;
-		t->next=temp;
-	} 
-	h=t->next;
-	t->next=temp;
+	//从小到大排序链表 
+	temp=list;
+	Node* m;
+	ElementType b;
+	while(t){
+		temp=list;
+		while(temp->next){
+			if(temp->data>temp->next->data){
+				b=temp->data;
+				temp->data=temp->next->data;
+				temp->next->data=b;
+			}
+			temp=temp->next;
+		} 
+		t=t->next; 
+	}
+	
 	
 	temp=list;
 	for(int i=0;i<13;i++){
