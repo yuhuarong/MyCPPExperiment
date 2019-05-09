757f61e6aeed5a89fe3cb50b5681f4e4929eec16
wcf <1533144693@qq.com>
2019-05-09 18:55:32 +0800

    更新实验内容

--- EXP/2018-2019(2) EXP/实验5 王程飞 201806061219/实验内容1/main.cpp
+++ EXP/2018-2019(2) EXP/实验5 王程飞 201806061219/实验内容1/main.cpp
@@ -3,9 +3,13 @@
 #include<algorithm>
 using namespace std;
 
+bool cmp(int a, int b){
+	return a>b;
+} 
+
 int main(){
 	int a[10]={56,34,3,67,89,55,22,43,12,9};
-	sort(a, a+10);
+	sort(a, a+10, cmp);
 	ofstream outfile("data.txt", ios::out);
 	if(outfile){
 		cout<<"write successed"<<endl;
